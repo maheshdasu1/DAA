303883ed6a1bb54095259bd2066d0848d19485b4
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-11-23 23:21:11 +0530

    Add files via upload

--- gcd&lcm.cpp
+++ gcd&lcm.cpp
@@ -1,26 +1,21 @@
-#include<stdio.h>
-
-int main()
-{
-    int num1, num2, gcd, lcm, count = 1, small;
-
+#include <stdio.h>
+int main() {
+    int num1, num2, gcd, lcm, i=1, small;
     printf("Enter 2 integer numbers\n");
     scanf("%d%d", &num1, &num2);
-
-    small = (num1 < num2) ? num1 : num2;
-
-    while(count <= small)
-    {
-        if(num1 % count == 0 && num2 % count == 0)
-        {
-            gcd = count;
-        }
-        count++;
+    if (num1 < num2) {
+        small = num1;
+    } else {
+        small = num2;
     }
-
-    lcm = ( num1 * num2 ) / gcd;
-
+    for(i=1;i<=small;i++) 
+	{
+        if (num1%i== 0 && num2 %i == 0) 
+		{
+            gcd=i;
+        }
+        
+    }
+    lcm = (num1 * num2) / gcd;
     printf("GCD = %d\nLCM = %d\n", gcd, lcm);
-
-    return 0;
 }
